90804391ca74d99ac80922d681424aad18b764fb
Aritra Sur Roy <aritra1997@hotmail.com>
2023-02-25 20:01:06 +0530

    get item from db

--- store-svc/repository/store_repository.go
+++ store-svc/repository/store_repository.go
@@ -1,8 +1,21 @@
 package repository
 
+import (
+	"fmt"
+
+	"github.com/Roy19/distributed-transaction-2pc/store-svc/db"
+	"github.com/Roy19/distributed-transaction-2pc/store-svc/models"
+	"gorm.io/gorm"
+)
+
 type StoreRepository struct {
 }
 
-func (s *StoreRepository) GetItem(itemID int) {
-
+func (s *StoreRepository) GetItem(itemID int) (int64, error) {
+	var item models.StoreItem
+	txOut := db.DB.First(&item, itemID)
+	if txOut.Error == gorm.ErrRecordNotFound {
+		return 0, fmt.Errorf("item not found")
+	}
+	return int64(item.ID), nil
 }
